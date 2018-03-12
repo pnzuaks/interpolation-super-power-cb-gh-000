79926d2102f67f4a1f68c786437db056517c50fe
Patricia Nykaza <pnykaza@calstatela.edu>
2018-03-12 16:35:24 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,11 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(array = [])
-  print "R: #{array[0]},"
-  print "O: #{array[1]},"
-  print "Y: #{array[2]},"
-  print "G: #{array[3]},"
-  print "B: #{array[4]},"
-  print "I: #{array[5]},"
-  print "V: #{array[6]},""\n"
+  print "R: #{array[0]}, "
+  print "O: #{array[1]}, "
+  print "Y: #{array[2]}, "
+  print "G: #{array[3]}, "
+  print "B: #{array[4]}, "
+  print "I: #{array[5]}, "
+  print "V: #{array[6]}, "
 end

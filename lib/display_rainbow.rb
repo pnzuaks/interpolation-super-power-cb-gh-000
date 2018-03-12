42795884fdf7b234c712fbb1b6ea0d36bbd2bfa2
Patricia Nykaza <pnykaza@calstatela.edu>
2018-03-12 16:40:51 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,5 +7,5 @@ def display_rainbow(colors = [])
   print "G: #{colors[3]}, "
   print "B: #{colors[4]}, "
   print "I: #{colors[5]}, "
-  print "V: #{colors[6]} "/n "
+  print "V: #{colors[6]}/n "
 end

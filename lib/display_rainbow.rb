eb2b1a3145c0f938376069179b99337ed6745b7e
Patricia Nykaza <pnykaza@calstatela.edu>
2018-03-12 16:33:48 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,10 +2,10 @@
 
 def display_rainbow(array = [])
   print "R: #{array[0]},"
-  puts "O: #{array[1]},"
-  puts "Y: #{array[2]},"
-  puts "G: #{array[3]},"
-  puts "B: #{array[4]},"
-  puts "I: #{array[5]},"
-  puts "V: #{array[6]},"
+  print "O: #{array[1]},"
+  print "Y: #{array[2]},"
+  print "G: #{array[3]},"
+  print "B: #{array[4]},"
+  print "I: #{array[5]},"
+  print "V: #{array[6]},"
 end
